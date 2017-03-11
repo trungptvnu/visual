b5e692d2ef8b8a88c0e0a3088ccc954ace18890e
Phạm Trung <trungptvnu@gmail.com>
2017-03-12 01:12:42 +0700

    vector

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -1,24 +1,24 @@
-#include <iostream>
-
-using namespace std;
-int main()
-{
-	int *array;
-	int number;
-	cout << " Moi nhap so luong phan tu " << " ";
-	cin >> number;
-	array = new int[number];
-	for (int i = 0; i < number; i++)
-	{
-		cin >> array[i];
-	}
-	cout << " mang la " << endl;
-	for (int i = 0; i < number; i++)
-	{
-		cout << array[i] << " " << endl;
-	}
-	cout << " hello world" << endl;
-	system("pause");
-	cin.get();
-	return 0;
-}
+//#include <iostream>
+//
+//using namespace std;
+//int main()
+//{
+//	int *array;
+//	int number;
+//	cout << " Moi nhap so luong phan tu " << " ";
+//	cin >> number;
+//	array = new int[number];
+//	for (int i = 0; i < number; i++)
+//	{
+//		cin >> array[i];
+//	}
+//	cout << " mang la " << endl;
+//	for (int i = 0; i < number; i++)
+//	{
+//		cout << array[i] << " " << endl;
+//	}
+//	cout << " hello world" << endl;
+//	system("pause");
+//	cin.get();
+//	return 0;
+//}
