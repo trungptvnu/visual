ac4c10eb3e106886745709f8e2232be7d54138b2
Phạm Trung <trungptvnu@gmail.com>
2017-03-11 21:42:50 +0700

    con tro mang

--- Project1/Project1/Source.cpp
+++ Project1/Project1/Source.cpp
@@ -2,8 +2,23 @@
 
 using namespace std;
 int main()
-
 {
+	int *array;
+	int number;
+	cout << " Moi nhap so luong phan tu " << " ";
+	cin >> number;
+	array = new int[number];
+	for (int i = 0; i < number; i++)
+	{
+		cin >> array[i];
+	}
+	cout << " mang la " << endl;
+	for (int i = 0; i < number; i++)
+	{
+		cout << array[i] << " " << endl;
+	}
 	cout << " hello world" << endl;
+	system("pause");
+	cin.get();
 	return 0;
 }
