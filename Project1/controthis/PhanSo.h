c160ae562361016d576e54c29f9dea62f59f7bee
trungptvnu <trungptvnu@gmail.com>
2017-03-13 10:26:24 +0700

    Update PhanSo.h

--- Project1/controthis/PhanSo.h
+++ Project1/controthis/PhanSo.h
@@ -6,8 +6,23 @@ using namespace std;
 
 class PhanSo
 {
+private:
+	int m_Tuso;
+	int m_Mauso;
 public:
 	PhanSo();
+	PhanSo(int &tuso, int &mauso);
 	virtual ~PhanSo();
+	void nhap();
+	int Gettuso();
+	int Getmauso();
+	friend PhanSo operator+ (const PhanSo &p1, const PhanSo &p2);
+	/*friend PhanSo operator- (const PhanSo &p1, const PhanSo &p2);
+	friend PhanSo operator=(const PhanSo &p1, const PhanSo &p2);
+	friend PhanSo operator* (const PhanSo &p1, const PhanSo &p2);
+	friend PhanSo operator/(const PhanSo &p1, const PhanSo &p2);*/
+
+
+
 };
 
